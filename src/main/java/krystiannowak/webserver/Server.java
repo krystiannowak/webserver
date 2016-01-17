1b3f714e7f27d76b7f61be4acc01f0ce223a9e53
krystiannowak <Krystian.Nowak@gmail.com>
2016-01-17 17:05:37 +0100

    final main params

--- src/main/java/krystiannowak/webserver/Server.java
+++ src/main/java/krystiannowak/webserver/Server.java
@@ -6,7 +6,7 @@ package krystiannowak.webserver;
  */
 public class Server {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // TODO Auto-generated method stub
 
     }
