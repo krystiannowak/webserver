50d67f71cbbcc330d471b02aac525183fa5bec34
krystiannowak <Krystian.Nowak@gmail.com>
2016-01-17 16:56:46 +0100

    tabs to spaces

--- src/main/java/krystiannowak/webserver/Server.java
+++ src/main/java/krystiannowak/webserver/Server.java
@@ -3,7 +3,7 @@ package krystiannowak.webserver;
 public class Server {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+        // TODO Auto-generated method stub
 
     }
 
