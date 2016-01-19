34408f297d53b9b7f51b2626b7bc1a47243abefc
krystiannowak <Krystian.Nowak@gmail.com>
2016-01-19 16:57:30 +0100

    hello world with rxjava and sockets

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ To build the webserver Docker image execute:
 
     mvn clean install docker:build
 
-and then to run the webserver within the Docker container:
+and then to run the webserver within the Docker container (serving on port 31337):
 
-	docker run -it --rm webserver
+	docker run -p 31337:31337 -it --rm webserver
--- src/main/java/krystiannowak/webserver/Server.java
+++ src/main/java/krystiannowak/webserver/Server.java
@@ -1,10 +1,43 @@
 package krystiannowak.webserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Throwables;
+
+import rx.Observable;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.Subscriptions;
+
 /**
  * @author krystiannowak
  *
  */
 public final class Server {
+
+    /**
+     * Static logger for main class.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(Server.class);
+
+    /**
+     * Default number of threads (if not overridden from configuration).
+     */
+    private static final int DEFAULT_NUMBER_OF_THREADS = 8;
+
+    /**
+     * Default port number for the server (if not overridden from
+     * configuration).
+     */
+    private static final int DEFAULT_PORT_NUMBER = 31337;
 
     /**
      * No instantiation possible.
@@ -19,8 +52,59 @@ public final class Server {
      *            arguments (not used)
      */
     public static void main(final String[] args) {
-        // TODO Auto-generated method stub
 
+        final int numberOfThreads = DEFAULT_NUMBER_OF_THREADS;
+
+        ExecutorService threadPoolExecutorService = Executors
+                .newFixedThreadPool(numberOfThreads);
+        Scheduler scheduler = Schedulers.from(threadPoolExecutorService);
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                threadPoolExecutorService.shutdownNow();
+            }
+        });
+
+        Observable.create(subscriber -> {
+            try {
+                final int port = DEFAULT_PORT_NUMBER;
+                final ServerSocket serverSocket = new ServerSocket(port);
+                subscriber.add(Subscriptions.create(() -> {
+                    try {
+                        LOG.info("closing socket on port {}", port);
+                        serverSocket.close();
+                    } catch (IOException e) {
+                        throw Throwables.propagate(e);
+                    }
+                }));
+                LOG.info("server on port {} started", port);
+                while (!subscriber.isUnsubscribed()) {
+                    try {
+                        Socket socket = serverSocket.accept();
+                        subscriber.onNext(socket);
+                    } catch (SocketTimeoutException e) {
+                        LOG.warn("timeout while accepting socket connection",
+                                e);
+                    } catch (Exception e) {
+                        subscriber.onError(e);
+                    }
+                }
+            } catch (IOException e) {
+                subscriber.onError(e);
+            }
+        }).flatMap(socket -> {
+            LOG.debug("receiving request from {}", socket);
+            try {
+                return Observable.just("hello world");
+            } catch (Exception e) {
+                return Observable.error(e);
+            }
+        }).observeOn(scheduler).subscribe(result -> {
+            LOG.info(result);
+        }, t -> {
+            LOG.error("an error occured", t);
+        });
     }
 
 }
